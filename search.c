b352f7df87344a2efcdec817e4f3304755b261d8
Evan Bay <116117905+etbay@users.noreply.github.com>
2026-04-23 09:56:25 -0700

    Added recursive returning of all paths to found files

--- search.c
+++ search.c
@@ -1,6 +1,21 @@
 #include "search.h"
 
-int search_ez_file(FILE *file, const char *search_term, int layer)
+DEF_ARRAY_LIST_SOURCE(char*, CharPtr, char_ptr);
+
+void get_all_paths_search(FILE *file, const char *search_term)
+{
+	ArrayListCharPtr paths;
+	init_arr_list_char_ptr(&paths);
+
+	search_ez_file(file, search_term, 0, &paths, "");
+
+	for (int i = 0; i < paths.length; i++)
+	{
+		printf("%s\n", paths.arr[i]);
+	}
+}
+
+void search_ez_file(FILE *file, const char *search_term, int layer, ArrayListCharPtr *paths, const char *current_path)
 {
 	char obj_name[MAX_FILE_NAME_SIZE];
 
@@ -14,47 +29,39 @@ int search_ez_file(FILE *file, const char *search_term, int layer)
             break;
         }
 
-        skip_whitespace(file);
         JCALL(enum EZObjectType obj_type = scan_header(file, MAX_FILE_NAME_SIZE, obj_name));
+
+        char *new_path = malloc(strlen(current_path) + strlen(obj_name) + 2);
+        strcpy(new_path, current_path);
+        if (strlen(current_path) > 0) strcat(new_path, "/");
+        strcat(new_path, obj_name);
 
         switch (obj_type)
         {
             case OBJ_DIRECTORY:
-                if (star_compare(search_term, obj_name))
-                { 
-                	printf("Found directory match: %s \n", obj_name);
-                	return 1;
-                }
-                int found;
-    			JCALL(found = search_dir(file, search_term, layer));
-    			if (found) return 1;
-                break;
-            case OBJ_FILE:
+            	skip_to_char(file, '{');
+    			fgetc(file);
+    			char *recurse_path = strdup(new_path);
                 if (star_compare(search_term, obj_name)) 
                 {
-                	printf("Found file match: %s \n", obj_name);
-                	return 1;
+                	push_arr_list_char_ptr(paths, new_path);
+					new_path = NULL;
                 }
-                search_file(file, search_term, layer);
+
+                search_ez_file(file, search_term, layer + 1, paths, recurse_path);
+                free(recurse_path);
+                break;
+            case OBJ_FILE:
+            	skip_to_char(file, '\"');
+			    JCALL(scan_till_char(file, '\"', layer + 1));
+			    fgetc(file);
+                if (star_compare(search_term, obj_name))
+                {
+                	push_arr_list_char_ptr(paths, new_path);
+                	new_path = NULL;
+                }
                 break;
         }
+        if (new_path) free(new_path);
     }
-
-    return 0;
-}
-
-int search_dir(FILE *file, const char *search_term, int layer)
-{
-    skip_to_char(file, '{');
-    fgetc(file);
-    int result;
-    JCALL(result = search_ez_file(file, search_term, layer + 1));
-    return result;
-}
-
-void search_file(FILE *file, const char *search_term, int layer)
-{
-    skip_to_char(file, '\"');
-    JCALL(scan_till_char(file, '\"', layer + 1));
-    fgetc(file);
 }
--- search.h
+++ search.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include "star_compare.h"
 #include "file_utils.h"
 #include "journaling.h"
+#define MAX_FILE_SIZE 1024
 
-int search_ez_file(FILE *file, const char *search_term, int layer);
-int search_dir(FILE *file, const char *search_term, int layer);
-void search_file(FILE *file, const char *search_term, int layer);
+DEF_ARRAY_LIST_HEADER(char*, CharPtr, char_ptr);
+
+void get_all_paths_search(FILE *file, const char *search_term);
+void search_ez_file(FILE *file, const char *search_term, int layer, ArrayListCharPtr *paths, const char *current_path);
